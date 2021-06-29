5e8cca245ab6416be01e527c8cc687d67200e053
Narek Hovsepyan <promentol@gmail.com>
2021-06-29 19:20:47 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,32 @@
 const get = require('lodash.get')
 
-function distanceBetweenPoints (p1, p2, name) {
+function distanceBetweenPoints (p1, p2, name, spheric = false) {
+	if(spheric) {
+		distanceBetweenPointsSpheric(
+			get(p1, name.y),
+			get(p1, name.x),
+			get(p2, name.y)
+			get(p2, name.x),
+			false
+		)
+	}
   return Math.abs(Math.sqrt(getDistanceY(p1, p2, name) + getDistanceX(p1, p2, name)))
+}
+
+function distanceBetweenPointsSpheric(lat1: number, lng1: number, lat2: number, lng2: number, miles = true) { // miles optional
+  if (typeof miles === "undefined"){miles=false;}
+  function deg2rad(deg: number){return deg * (Math.PI/180);}
+  function square(x: number){return Math.pow(x, 2);}
+  var r=6371; // radius of the earth in km
+  lat1=deg2rad(lat1);
+  lat2=deg2rad(lat2);
+  var lat_dif=lat2-lat1;
+  var lng_dif=deg2rad(lng2-lng1);
+  var a=square(Math.sin(lat_dif/2))+Math.cos(lat1)*Math.cos(lat2)*square(Math.sin(lng_dif/2));
+  var d=2*r*Math.asin(Math.sqrt(a));
+
+  var ans = miles ? d * 0.621371 : d;
+  return Math.round((ans + Number.EPSILON) * 100) / 100
 }
 
 function getDistanceY (p1, p2, name) {
@@ -25,8 +50,8 @@ function sortByDistance (origin, points, opts = {}) {
   const newPoints = points.slice()
 
   newPoints.sort(function (a, b) {
-    a.distance = distanceBetweenPoints(origin, a, names)
-    b.distance = distanceBetweenPoints(origin, b, names)
+    a.distance = distanceBetweenPoints(origin, a, names, opts.spheric)
+    b.distance = distanceBetweenPoints(origin, b, names, opts.spheric)
 
     return a.distance - b.distance
   })
